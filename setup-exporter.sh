eb5cb47c2f5013e7bcade0fd6a07cb4abcca618a
Alexander Krischan <akrischan@vmlab.at>
2024-04-04 13:53:47 +0200

    fix naming scheme

--- setup-exporter.sh
+++ setup-exporter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-go mod init monit2prom
+go mod init monit_exporter
 https_proxy=http://10.50.1.4:8081 go get github.com/prometheus/client_golang/prometheus/
 https_proxy=http://10.50.1.4:8081 go get github.com/prometheus/log
 https_proxy=http://10.50.1.4:8081 go get github.com/prometheus/client_golang/prometheus/promhttp

08d7a0e77528dd40359a831924f0d763e9340356
Alexander Krischan <akrischan@vmlab.at>
2024-04-04 14:45:51 +0200

    Create systemd service & expand setup

--- setup-exporter.sh
+++ setup-exporter.sh
@@ -1,8 +1,42 @@
 #!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+echo "Set http/s_proxy? (e.g. http://ip:port)"
+read -r proxy
+if [[ "$proxy" == *"http://"* ]]; then
+    export http_proxy=$proxy
+    export https_proxy=$proxy
+    goProxy = "https_proxy=$proxy"
+fi
+
+echo "Install golang 1.21.6? (Y/n)"
+read -r golang
+
+if [[ "$golang" == "y" ]]; then
+    bash setup-go.sh
+fi
+
 go mod init monit_exporter
-https_proxy=http://10.50.1.4:8081 go get github.com/prometheus/client_golang/prometheus/
-https_proxy=http://10.50.1.4:8081 go get github.com/prometheus/log
-https_proxy=http://10.50.1.4:8081 go get github.com/prometheus/client_golang/prometheus/promhttp
-https_proxy=http://10.50.1.4:8081 go get github.com/spf13/viper
-https_proxy=http://10.50.1.4:8081 go get golang.org/x/net/html/charset
+echo "$goProxy go get github.com/prometheus/client_golang/prometheus/" | bash
+echo "$goProxy go get github.com/prometheus/log" | bash
+echo "$goProxy go get github.com/prometheus/client_golang/prometheus/promhttp" | bash
+echo "$goProxy go get github.com/spf13/viper" | bash
+echo "$goProxy go get golang.org/x/net/html/charset" | bash
 go build
+cp monit_exporter /usr/local/go/bin/
+
+
+echo "Install systemd service? (Y/n)"
+read -r systemd
+if [[  "$systemd" == "y" ]]; then
+  cp monit_exporter.service /etc/systemd/system/
+  systemctl enable monit_exporter --now
+fi
+
+# clean up
+export http_proxy=""
+export https_proxy=""
